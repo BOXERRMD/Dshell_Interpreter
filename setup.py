a65dd343e0676eebdade5967f0cce269f991613c
BOXER <vagabonwalybi@gmail.com>
2025-08-22 17:12:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dshellInterpreter",
-    version="0.2.10.6",
+    version="0.2.11",
     author="Chronos",
     author_email="vagabonwalybi@gmail.com",
     description="A Discord bot interpreter for creating custom commands and automations.",
