8ec8d3a431b43d7a2fefef3f8cf0eefdd477037a
BOXER <vagabonwalybi@gmail.com>
2026-01-05 17:17:50 +0100

    1.0.0.0 add scope

--- Dshell/_DshellInterpreteur/dshell_interpreter.py
+++ Dshell/_DshellInterpreteur/dshell_interpreter.py
@@ -1,7 +1,7 @@
 from .._DshellTokenizer.dshell_token_type import Token
 from .._DshellTokenizer.dshell_token_type import DshellTokenType as DTT
 from .._DshellInterpreteur.errors import DshellInterpreterStopExecution
-from Dshell.full_import import TypeVar, Union, Optional, Any, Callable, deepcopy, sleep, findall
+from Dshell.full_import import TypeVar, Union, Optional, Any, Callable, sleep, findall
 from .._DshellParser.ast_nodes import *
 from Dshell.full_import import AutoShardedBot, Interaction, Message, PrivateChannel, Embed
 from Dshell.full_import import EasyModifiedViews
@@ -13,6 +13,7 @@ from .utils_interpreter import get_params, eval_expression, eval_expression_inli
 from ..DISCORD_COMMANDS.dshell_embed import build_embed, rebuild_embed
 from ..DISCORD_COMMANDS.dshell_ui import build_ui, rebuild_ui
 from ..DISCORD_COMMANDS.utils.utils_permissions import build_permission
+from .dshell_scope import Scope, new_scope
 
 from Dshell.full_import import Union
 
@@ -47,7 +48,8 @@ class DshellInterpreteur:
             self.ast: list[ASTNode] = code.body
 
         message = ctx.message if isinstance(ctx, Interaction) else ctx
-        self.env: dict[str, Any] = {
+        self.env: Scope = Scope()
+        self.env.update({
             '__ret__': None,  # environment variables, '__ret__' is used to store the return value of commands
 
             '__author__': message.author.id,
@@ -100,12 +102,16 @@ class DshellInterpreteur:
             '__guild_forum_channels__': ListNode([channel.id for channel in message.channel.guild.forum_channels]),
             '__guild_channels_count__': len(message.channel.guild.channels),
 
-        } if message is not None and not debug else {'__ret__': None} # {} is used in debug mode, when ctx is None
+        } if message is not None and not debug else {'__ret__': None}) # {} is used in debug mode, when ctx is None
+
         if vars_env is not None: # add the variables to the environment
             self.env.update(vars_env)
+
         self.vars = vars or ''
         self.ctx: context = ctx
+
         dshell_cached_messages.set(dict()) # save all messages view in the current scoop
+
         if debug:
             print_ast(self.ast)
 
@@ -132,8 +138,8 @@ class DshellInterpreteur:
 
             if isinstance(node, CommandNode):
                 result = await call_function(dshell_commands[node.name], node.body, self)
-                self.env[f'__{node.name}__'] = result # return value of the command
-                self.env['__ret__'] = result  # global return variable for all commands
+                self.env.set(f'__{node.name}__', result) # return value of the command
+                self.env.set('__ret__', result)  # global return variable for all commands
 
             elif isinstance(node, ParamNode):
                 params = await get_params(node, self)
@@ -156,48 +162,46 @@ class DshellInterpreteur:
                     await self.execute(node.else_body.body)
 
             elif isinstance(node, LoopNode):
-                self.env[node.variable.name.value] = 0
+                self.env.set(node.variable.name.value, 0)
                 for i in DshellIterator(await eval_expression(node.variable.body, self)):
-                    self.env[node.variable.name.value] = i
-                    c = deepcopy(node.body)
-                    await self.execute(c)
-                    del c
+                    with new_scope(self, {node.variable.name.value: i}):
+                        await self.execute(node.body)
 
             elif isinstance(node, VarNode):
 
                 first_node = node.body[0]
                 if isinstance(first_node, IfNode):
-                    self.env[node.name.value] = await eval_expression_inline(first_node, self)
+                    self.env.set(node.name.value, await eval_expression_inline(first_node, self))
 
                 elif isinstance(first_node, EmbedNode):
                     # rebuild the embed if it already exists
-                    if node.name.value in self.env and isinstance(self.env[node.name.value], Embed):
-                        self.env[node.name.value] = await rebuild_embed(self.env[node.name.value], first_node.body, first_node.fields, self)
+                    if self.env.contains(node.name.value) and isinstance(self.env.get(node.name.value), Embed):
+                        self.env.set(node.name.value, await rebuild_embed(self.env.get(node.name.value), first_node.body, first_node.fields, self))
                     else:
-                        self.env[node.name.value] = await build_embed(first_node.body, first_node.fields, self)
+                        self.env.set(node.name.value, await build_embed(first_node.body, first_node.fields, self))
 
                 elif isinstance(first_node, PermissionNode):
                     # rebuild the permissions if it already exists
-                    if node.name.value in self.env and isinstance(self.env[node.name.value], dict):
-                        self.env[node.name.value].update(await build_permission(first_node.body, self))
+                    if self.env.contains(node.name.value) and isinstance(self.env.get(node.name.value), dict):
+                        self.env.get(node.name.value).update(await build_permission(first_node.body, self))
                     else:
-                        self.env[node.name.value] = await build_permission(first_node.body, self)
+                        self.env.set(node.name.value, await build_permission(first_node.body, self))
 
                 elif isinstance(first_node, UiNode):
                     # rebuild the UI if it already exists
-                    if node.name.value in self.env and isinstance(self.env[node.name.value], EasyModifiedViews):
-                        self.env[node.name.value] = await rebuild_ui(first_node, self.env[node.name.value], self)
+                    if self.env.contains(node.name.value) and isinstance(self.env.get(node.name.value), EasyModifiedViews):
+                        self.env.set(node.name.value, await rebuild_ui(first_node, self.env.get(node.name.value), self))
                     else:
-                        self.env[node.name.value] = await build_ui(first_node, self)
+                        self.env.set(node.name.value, await build_ui(first_node, self))
 
                 elif isinstance(first_node, CodeNode):
-                    if node.name.value in self.env and isinstance(self.env[node.name.value], first_node):
-                        self.env[node.name.value].update(first_node)
+                    if self.env.contains(node.name.value) and isinstance(self.env.get(node.name.value), first_node):
+                        self.env.get(node.name.value).update(first_node)
                     else:
-                        self.env[node.name.value] = first_node
+                        self.env.set(node.name.value, first_node)
 
                 else:
-                    self.env[node.name.value] = await eval_expression(node.body, self)
+                    self.env.set(node.name.value, await eval_expression(node.body, self))
 
             elif isinstance(node, SleepNode):
                 sleep_time = await eval_expression(node.body, self)
@@ -213,7 +217,7 @@ class DshellInterpreteur:
                 await eval_CodeNode(node, self)
 
             elif isinstance(node, ReturnNode):
-                self.env['__ret__'] = await eval_expression(node.body, self)
+                self.env.set('__ret__', await eval_expression(node.body, self))
 
             elif isinstance(node, EndNode):
                 if await self.eval_data_token(node.error_message):
@@ -242,18 +246,18 @@ class DshellInterpreteur:
             return ListNode(
                 [await self.eval_data_token(tok) for tok in token.value])  # token.value contient déjà une liste de Token
         elif token.type == DTT.IDENT:
-            if token.value in self.env.keys():
-                if isinstance(self.env[token.value], CodeNode):
-                    return deepcopy(self.env[token.value])
-                return self.env[token.value]
-            return token.value
+            try:
+                return self.env.get(token.value)
+            except KeyError:
+                return token.value
         elif token.type == DTT.EVAL_GROUP:
             await self.execute(parse([token.value], StartNode([]))[0]) # obliger de parser car ce il n'est pas dejà un AST
-            return self.env['__ret__']
+            return self.env.get('__ret__')
         elif token.type == DTT.STR:
-            for match in findall(rf"\$({'|'.join(self.env.keys())})", token.value):
-                token.value = token.value.replace('$' + match, str(self.env[match]))
-            return token.value
+            temp = token.value
+            for match in findall(rf"\$({'|'.join(self.env.keys())})", temp):
+                temp = temp.replace('$' + match, str(self.env.get(match)))
+            return temp
         else:
             return token.value  # fallback
 
@@ -287,16 +291,11 @@ async def eval_CodeNode(eval_node: EvalNode, interpreter: DshellInterpreteur):
     argscommand = await regroupe_commandes(eval_node.argsNode.body, interpreter)
     kwargs = argscommand.get_dict_parameters()
     kwargs.pop('*', None)
-    env_temp_variables = interpreter.env.copy()  # save previous variables
 
-    interpreter.env.update(kwargs)
-    await interpreter.execute(codeNode.body)
+    with new_scope(interpreter, kwargs):
+        await interpreter.execute(codeNode.body)
 
-    interpreter.env.clear()
-    interpreter.env.update(env_temp_variables)  # restore previous variables
-    del codeNode
-
-    return interpreter.env['__ret__']
+    return interpreter.env.get('__ret__')
 
 
 class DshellIterator:
