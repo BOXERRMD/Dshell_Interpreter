b938ced927f50507843381ce6b19c23d1d9dd3b9
BOXER <vagabonwalybi@gmail.com>
2026-06-05 21:43:45 +0200

    fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dshellInterpreter",
-    version="1.2.0.0",
+    version="1.2.0.1",
     author="Chronos",
     author_email="vagabonwalybi@gmail.com",
     description="A Discord bot interpreter for creating custom commands and automations.",
