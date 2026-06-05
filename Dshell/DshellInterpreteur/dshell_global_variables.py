714f5dacd627cd787c35c00733e98256ef0936de
BOXER <vagabonwalybi@gmail.com>
2026-06-05 22:32:08 +0200

    add more memory to list and string

--- Dshell/DshellInterpreteur/dshell_global_variables.py
+++ Dshell/DshellInterpreteur/dshell_global_variables.py
@@ -1,7 +1,7 @@
 from sys import getsizeof
 
-MAX_STR_SIZE = 7_000 # bytes
-MAX_LIST_SIZE = 150_000 # bytes
+MAX_STR_SIZE = 10_000 # bytes
+MAX_LIST_SIZE = 500_000 # bytes
 # Constants for sleep time limits
 MAX_SLEEP_TIME_SECONDS = 3_600  # 1 hour
 MIN_SLEEP_TIME_SECONDS = 1
